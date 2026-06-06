905c8a79b69cba02513917b5e11201455aef796d
bekyman <bieky47@gmail.com>
2026-06-06 21:10:56 +0300

    Create and Update userController.js under Controllers folder

---
+++ backend/controllers/userController.js
@@ -0,0 +1,11 @@
+const User = require('../models/User');
+
+exports.createUser = async (req, res) => {
+    try {
+        const newUser = new User(req.body);
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
