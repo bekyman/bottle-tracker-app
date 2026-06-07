47b74bb88a390ada33f116e276b79144e763b0ad
bekyman <bieky47@gmail.com>
2026-06-07 19:02:39 +0300

    Update the server.js by incorporating the routes

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,14 @@ const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
 
+const userRoutes = require('./routes/userRoutes');
+
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use('/api/users', userRoutes);
 
 app.get('/', (req, res) => {
     res.send('Bottle Tracker API is running...');
