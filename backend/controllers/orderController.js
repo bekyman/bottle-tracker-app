aaa033407d585a5ebbc8de05d05ed9e0c776774c
bekyman <bieky47@gmail.com>
2026-06-09 21:44:12 +0300

    Create and Update orderController.js

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,38 @@
+const Order = require('../models/Order');
+
+exports.getAllOrders = async (req, res) => {
+    try {
+        const orders = await Order.find().populate('userId productId'); // Optional: populate if you have refs
+        res.status(200).json(orders);
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+exports.createOrder = async (req, res) => {
+    try {
+        const newOrder = new Order(req.body);
+        const savedOrder = await newOrder.save();
+        res.status(201).json(savedOrder);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.updateOrder = async (req, res) => {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(req.params.id, req.body, { new: true });
+        res.json(updatedOrder);
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+exports.deleteOrder = async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.json({ message: "Order deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
